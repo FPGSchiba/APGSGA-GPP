d30d4cf2a5da0583a417d2d4de0ffbc2dcc54151
FPGSchiba <craftzockerlp@gmail.com>
2020-07-08 09:49:37 +0200

    Update Program.cs

--- APGSGA-GPP_App/APGSGA-GPP_App/Program.cs
+++ APGSGA-GPP_App/APGSGA-GPP_App/Program.cs
@@ -17,8 +17,6 @@ namespace APGSGA_GPP_App
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-
-            Console.WriteLine("Das SaS");
         }
     }
 }
